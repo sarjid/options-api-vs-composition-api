8fdf6f1db5ddb505a779c54aff2377c253a9dcc8
sarjid <sarjid777gmail.com>
2022-08-01 04:19:41 +0600

    props and component

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,25 @@
-<template>
-  <div>
-    {{ name1 }}
-    {{ name2 }}
-    {{ fullName }}
+<script >
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from "./components/HelloWorld.vue";
 
-    <button @click="changeFunction">Change</button>
-  </div>
-</template>
-
-<script setup>
-import { ref, onMounted, computed } from "vue";
-// export default {
-//   data() {
-//     return {
-//       name1: "jishan",
-//       name2: "Hasan",
-//     };
-//   },
-
-//   methods: {
-//     changeFunction() {
-//       this.name1 = "Hasan";
-//       this.name2 = "testing..";
-//     },
-//   },
-
-//   // mounted() {
-//   //   this.changeFunction();
-//   // },
-
-//   computed:{
-//     fullName(){
-//       return this.name1 + this.name2;
-//     }
-//   }
-// };
-
-const name1 = ref("Jishan");
-const name2 = ref("Hasan");
-
-const changeFunction = () => {
-  name1.value = "Hasan";
-  name2.value = "testing..";
-};
-
-// onMounted(() => {
-//   changeFunction();
-// })
-
-const fullName = computed(() => {
-  return name1.value + name2.value;
-});
+export default {
+  components:{HelloWorld}
+}
 </script>
 
-<style lang=""></style>
+<template>
+  <img alt="Vue logo" src="/vite.svg" />
+  <HelloWorld msg="Hello Vue + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,24 @@
 <script setup>
-import { ref } from 'vue'
+// defineProps({
+//   msg: String
+// })
 
-defineProps({
-  msg: String
-})
+// export default {
+//   props: ["msg"],
+// };
 
-const count = ref(0)
+const props = defineProps({
+  msg: String,
+});
+
+console.log(props.msg);
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <h1>{{ props.msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
+    -->
     <p>
       Edit
       <code>components/HelloWorld.vue</code> to test HMR
