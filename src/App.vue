14aa40eab40420968f4456fbea66b80e6cf155d1
sarjid <sarjid777gmail.com>
2022-08-01 01:40:30 +0600

    v model ref

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template >
+  <div>
+    <input type="text" placeholder="your name" v-model="name">
+    <h3>your name is: {{ name }}</h3>
+  </div>
+</template>
+
+
+<script setup>
+import {ref} from 'vue';
+// export default {
+  // data() {
+  //   return {
+  //     name: ''
+  //   }
+  // },
+
+  // setup() {
+    const name = ref("");
+
+
+    
+  // }
+// }
+</script>
+<style lang="">
+  
+</style>
