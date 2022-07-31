bc25c160ea014b6e1089d89028e8ab0c323d08d5
sarjid <sarjid777gmail.com>
2022-08-01 04:01:57 +0600

    next part

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,56 @@
-<template >
+<template>
   <div>
-  
+    {{ name1 }}
+    {{ name2 }}
+    {{ fullName }}
 
-    <button @click="increment">Count {{ counter }}</button>
+    <button @click="changeFunction">Change</button>
   </div>
 </template>
 
-
 <script setup>
-import {reactive, ref} from 'vue';
+import { ref, onMounted, computed } from "vue";
 // export default {
-  // data() {
-  //   return {
-  //     counter: 0
-  //   }
-  // },
+//   data() {
+//     return {
+//       name1: "jishan",
+//       name2: "Hasan",
+//     };
+//   },
 
-  // methods: {
-  //   increment(){
-  //     this.counter += 1;
-  //   }
-  // },
+//   methods: {
+//     changeFunction() {
+//       this.name1 = "Hasan";
+//       this.name2 = "testing..";
+//     },
+//   },
 
-  // setup() {
-    const counter = ref(0);
-    const test = reactive(0);
+//   // mounted() {
+//   //   this.changeFunction();
+//   // },
 
-    const increment = () =>{
-      counter.value += 1;
-    };
-    
+//   computed:{
+//     fullName(){
+//       return this.name1 + this.name2;
+//     }
+//   }
+// };
 
-  
+const name1 = ref("Jishan");
+const name2 = ref("Hasan");
 
-    // return {counter,increment}
+const changeFunction = () => {
+  name1.value = "Hasan";
+  name2.value = "testing..";
+};
 
+// onMounted(() => {
+//   changeFunction();
+// })
 
-    
-  // }
-// }
+const fullName = computed(() => {
+  return name1.value + name2.value;
+});
 </script>
-<style lang="">
-  
-</style>
+
+<style lang=""></style>
